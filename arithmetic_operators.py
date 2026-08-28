11ad4bc881b3a566529d8a13f29c5b840beeb23a
MuhammedAbrar17 <abuabrar520724@gmail.com>
2026-08-28 21:31:09 +0530

    arithemeti_operator

---
+++ arithmetic_operators.py
@@ -0,0 +1,28 @@
+#addition 
+
+value = 5+3
+print(value)
+
+#Subtraction
+value2 = 8-2
+print(value2)
+
+#Multiplication
+value3 = 2*2
+print(value3)
+
+#Division
+value4 = 3/2
+print(value4)
+
+#Floor Division
+value5 = 6//2
+print(value5)
+
+#MOULUS
+value6 = 4%2
+print(value6)
+
+#Exponentaion
+value7 = 10**3
+print(value7)
--- python.py
+++ python.py
@@ -1,8 +1,10 @@
-
+#Creating Variables
 age = 30
 Name = "Abrar"
 
+#Changing the Value of variable
 age = 24
 Name = "Abu"
 
+#Printing out the result of variables
 print(age,Name, sep="\n")
